8a1437e60165c2ce0fb2af356a2f6f8dddb5afcb
Danny Lin <danny@kdrag0n.dev>
2021-05-27 10:48:53 -0700

    content: Add passthrough filter implementation

---
+++ src/main/kotlin/dev/kdrag0n/patreondl/content/filters/PassthroughFilter.kt
@@ -0,0 +1,19 @@
+package dev.kdrag0n.patreondl.content.filters
+
+import dev.kdrag0n.patreondl.config.Config
+import io.ktor.application.*
+import java.io.InputStream
+import java.io.OutputStream
+
+class PassthroughFilter(
+    environment: ApplicationEnvironment,
+    config: Config,
+) : ContentFilter {
+    override fun writeData(call: ApplicationCall, ins: InputStream, os: OutputStream) {
+        ins.copyTo(os)
+    }
+
+    override fun getFinalLength(call: ApplicationCall, len: Long): Long {
+        return len
+    }
+}
