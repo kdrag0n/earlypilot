f7c28372c5513bcba8df486d4e9b88e3c6c95f15
Danny Lin <danny@kdrag0n.dev>
2021-06-10 14:36:39 -0700

    email: Add download count to dunning templates

--- src/main/kotlin/dev/kdrag0n/patreondl/external/email/DunningMailer.kt
+++ src/main/kotlin/dev/kdrag0n/patreondl/external/email/DunningMailer.kt
@@ -1,8 +1,13 @@
 package dev.kdrag0n.patreondl.external.email
 
 import dev.kdrag0n.patreondl.config.Config
+import dev.kdrag0n.patreondl.data.AccessType
+import dev.kdrag0n.patreondl.data.DownloadEvent
+import dev.kdrag0n.patreondl.data.DownloadEvents
 import dev.kdrag0n.patreondl.data.User
 import dev.kdrag0n.patreondl.external.email.EmailTemplates.Companion.execute
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 class DunningMailer(
     private val config: Config,
@@ -18,9 +23,16 @@ class DunningMailer(
             else -> error("Invalid reminder ID")
         }
 
+        val downloadCount = newSuspendedTransaction {
+            DownloadEvent.find {
+                (DownloadEvents.accessType eq AccessType.USER) and (DownloadEvents.tag eq user.id.value)
+            }.count()
+        }
+
         val messageText = template.execute(mapOf(
             "config" to config,
             "user" to user,
+            "downloadCount" to downloadCount,
         ))
 
         mailer.sendEmail(user, subject, messageText, personal = reminderId >= 3)
